528192559914d289abcb22cdbf74a105f2091d97
lenny <lennyrappro@gmail.com>
2025-04-12 15:39:58 +0200

    🆕 Add stations & info

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,38 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+    // Appel à la méthode getAllStations pour récupérer les données de l'API
+        $stationData = $this->getAllStations();
+
+    // Passer les données des stations au template Twig
         return $this->render('home/index.html.twig', [
+            'stations' => $stationData['records'], // On suppose que l'API retourne 'records'
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/api/stations', name: 'api_stations')]
+    public function getAllStations(): array
+    {
+    // Création du client HTTP
+        $client = HttpClient::create();
+
+    // Appel à l'API pour récupérer les données de disponibilité des stations Vélib
+        $response = $client->request('GET', 'https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=1471');
+
+    // Retourner les données sous forme de tableau (pas en JSON directement)
+        return $response->toArray();
+    }
 }
+
+
